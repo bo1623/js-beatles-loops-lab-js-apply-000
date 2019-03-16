3f5e72efb9e57c03f020ec953418eb4d56c20085
Oh Boon Sim <boonsim.oh@blackrock.com>
2019-03-16 07:32:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ function theBeatlesPlay(musicians, instruments){
   return result
 }
 
+var facts=["He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"]
+
 function johnLennonFacts(facts){
   var result=[]
   var i
   while(i<facts.length){
     result.push(facts[i]+'!!!')
   }
+  return result
 }
 
 
