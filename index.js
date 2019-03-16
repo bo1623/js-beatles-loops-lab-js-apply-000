ad11d6840b3655c9244928fc18e61b6a9afcff5f
Oh Boon Sim <boonsim.oh@blackrock.com>
2019-03-16 07:31:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 function theBeatlesPlay(musicians, instruments){
   var result=[]
   var i
