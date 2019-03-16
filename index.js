3282208fceaac3096dba76397c6796523e1b3bec
Oh Boon Sim <boonsim.oh@blackrock.com>
2019-03-16 07:15:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // add solution here
 function theBeatlesPlay(musician, instrument){
   var result=[]
+  var i
+  for (i-0;i<musician.length;i++){
+    
+  }
   
+}
+
+var i;
+for (i = 0; i < cars.length; i++) { 
+  text += cars[i] + "<br>";
 }
